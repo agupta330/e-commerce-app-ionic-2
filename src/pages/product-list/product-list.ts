c2d3a6e6394cea908459d2d8e5b9c7df86fa71a3
Andre Luiz da Silva <andreluiz1013@gmail.com>
2017-05-21 21:09:10 -0300

    README.md

---
+++ src/pages/product-list/product-list.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+/**
+ * Generated class for the ProductListPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-product-list',
+  templateUrl: 'product-list.html',
+})
+export class ProductListPage {
+
+  public products = [];
+
+  constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams,
+      public http: Http
+   ){}
+
+  ionViewDidLoad() {
+    this.http.get('http://127.0.0.1:8000/api/admin/products')
+      .toPromise().then(response => this.products = response.json())
+
+  }
+}
